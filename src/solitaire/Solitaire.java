941cf3228ef6e21f004bb02fba240974fea7043e
Stephen Kuo <kuo.stephen72790@gmail.com>
2014-07-13 10:38:39 -0400

    minor

--- src/solitaire/Solitaire.java
+++ src/solitaire/Solitaire.java
@@ -9,12 +9,12 @@ import java.util.NoSuchElementException;
  * This class implements a simplified version of Bruce Schneier's Solitaire Encryption algorithm.
  */
 public class Solitaire {
-	
+
 	/**
 	 * Circular linked list that is the deck of cards for encryption
 	 */
 	CardNode deckRear;
-	
+
 	/**
 	 * Makes a shuffled deck of cards for encryption. The deck is stored in a circular
 	 * linked list, whose last node is pointed to by the field deckRear
@@ -26,52 +26,52 @@ public class Solitaire {
 		for (int i=0; i < cardValues.length; i++) {
 			cardValues[i] = i+1;
 		}
-		
+
 		// shuffle the cards
 		Random randgen = new Random();
- 	        for (int i = 0; i < cardValues.length; i++) {
-	            int other = randgen.nextInt(28);
-	            int temp = cardValues[i];
-	            cardValues[i] = cardValues[other];
-	            cardValues[other] = temp;
-	        }
-	     
- 	    // CREATION PART    
-	    // create a circular linked list from this deck and make deckRear point to its last node
-	    CardNode cn = new CardNode();
-	    cn.cardValue = cardValues[0];
-	    cn.next = cn;
-	    deckRear = cn;
-	    for (int i=1; i < cardValues.length; i++) {
-	    	cn = new CardNode();
-	    	cn.cardValue = cardValues[i];
-	    	cn.next = deckRear.next;
-	    	deckRear.next = cn;
-	    	deckRear = cn;
-	    }
+		for (int i = 0; i < cardValues.length; i++) {
+			int other = randgen.nextInt(28);
+			int temp = cardValues[i];
+			cardValues[i] = cardValues[other];
+			cardValues[other] = temp;
+		}
+
+		// CREATION PART    
+		// create a circular linked list from this deck and make deckRear point to its last node
+		CardNode cn = new CardNode();
+		cn.cardValue = cardValues[0];
+		cn.next = cn;
+		deckRear = cn;
+		for (int i=1; i < cardValues.length; i++) {
+			cn = new CardNode();
+			cn.cardValue = cardValues[i];
+			cn.next = deckRear.next;
+			deckRear.next = cn;
+			deckRear = cn;
+		}
 	}
-	
+
 	/**
 	 * Makes a circular linked list deck out of values read from scanner.
 	 */
 	public void makeDeck(Scanner scanner) 
-	throws IOException {
+			throws IOException {
 		CardNode cn = null;
 		if (scanner.hasNextInt()) {
 			cn = new CardNode();
-		    cn.cardValue = scanner.nextInt();
-		    cn.next = cn;
-		    deckRear = cn;
+			cn.cardValue = scanner.nextInt();
+			cn.next = cn;
+			deckRear = cn;
 		}
 		while (scanner.hasNextInt()) {
 			cn = new CardNode();
-	    	cn.cardValue = scanner.nextInt();
-	    	cn.next = deckRear.next;
-	    	deckRear.next = cn;
-	    	deckRear = cn;
+			cn.cardValue = scanner.nextInt();
+			cn.next = deckRear.next;
+			deckRear.next = cn;
+			deckRear = cn;
 		}
 	}
-	
+
 	/**
 	 * Implements Step 1 - Joker A - on the deck.
 	 */
@@ -87,40 +87,40 @@ public class Solitaire {
 		node.cardValue = node.next.cardValue;	
 		node.next.cardValue = temp;
 		//checking to see if deck is right
-//		node = deckRear;
-//		for(int i = 0; i < 28; i++){
-//			node = node.next;
-//			System.out.print(node.cardValue + " ");
-//		}
+		//		node = deckRear;
+		//		for(int i = 0; i < 28; i++){
+		//			node = node.next;
+		//			System.out.print(node.cardValue + " ");
+		//		}
 		return;
 	}
-	
+
 	/**
 	 * Implements Step 2 - Joker B - on the deck.
 	 */
 	void jokerB() {
-	    if (deckRear == null){
-	    	return;
-	    }
-	    CardNode node = deckRear;
-	    while(node.cardValue != 28){
-	    	node = node.next;
-	    }
-	    for (int i = 0; i < 2 ;i++){
-	    	int temp = node.cardValue;
-	    	node.cardValue = node.next.cardValue;
-	    	node.next.cardValue = temp;
-	    	node = node.next;
-	    }
-	    //checking to see if deck is right
-//	    node = deckRear;
-//	    for(int i = 0; i < 28; i++){
-//			node = node.next;
-//			System.out.print(node.cardValue + " ");
-//		}
-	    return;
+		if (deckRear == null){
+			return;
+		}
+		CardNode node = deckRear;
+		while(node.cardValue != 28){
+			node = node.next;
+		}
+		for (int i = 0; i < 2 ;i++){
+			int temp = node.cardValue;
+			node.cardValue = node.next.cardValue;
+			node.next.cardValue = temp;
+			node = node.next;
+		}
+		//checking to see if deck is right
+		//	    node = deckRear;
+		//	    for(int i = 0; i < 28; i++){
+		//			node = node.next;
+		//			System.out.print(node.cardValue + " ");
+		//		}
+		return;
 	}
-	
+
 	/**
 	 * Implements Step 3 - Triple Cut - on the deck.
 	 */
@@ -151,7 +151,7 @@ public class Solitaire {
 			deckRear = previous;
 			return;
 		}
-		
+
 		CardNode node = deckRear.next;
 		CardNode prevnode = null;
 		while(node.cardValue != 27 && node.cardValue != 28){
@@ -165,20 +165,20 @@ public class Solitaire {
 			node = node.next;
 		}
 		CardNode top2 = node.next;
-		
+
 		node.next = deckRear.next;
 		deckRear.next = firstJoker;
 		deckRear = tail1;
 		tail1.next = top2;
-	    //checking to see if deck is right
-//		node = deckRear;
-//	    for(int i = 0; i < 28; i++){
-//			node = node.next;
-//			System.out.print(node.cardValue + " ");
-//		}
+		//checking to see if deck is right
+		//		node = deckRear;
+		//	    for(int i = 0; i < 28; i++){
+		//			node = node.next;
+		//			System.out.print(node.cardValue + " ");
+		//		}
 		return;
 	}
-	
+
 	/**
 	 * Implements Step 4 - Count Cut - on the deck.
 	 */
@@ -202,21 +202,21 @@ public class Solitaire {
 		node.next = deckRear;
 		deckRear.next = newtop;
 		//checking to see if deck is right
-//		node = deckRear;
-//	    for(int k = 0; k < 28; k++){
-//			node = node.next;
-//			System.out.print(node.cardValue + " ");
-//		}
+		//		node = deckRear;
+		//	    for(int k = 0; k < 28; k++){
+		//			node = node.next;
+		//			System.out.print(node.cardValue + " ");
+		//		}
 		return;
 	}
-	
-        /**
-         * Gets a key. Calls the four steps - Joker A, Joker B, Triple Cut, Count Cut, then
-         * counts down based on the value of the first card and extracts the next card value 
-         * as key, but if that value is 27 or 28, repeats the whole process.
-         * 
-         * @return Key between 1 and 26
-         */
+
+	/**
+	 * Gets a key. Calls the four steps - Joker A, Joker B, Triple Cut, Count Cut, then
+	 * counts down based on the value of the first card and extracts the next card value 
+	 * as key, but if that value is 27 or 28, repeats the whole process.
+	 * 
+	 * @return Key between 1 and 26
+	 */
 	int getKey() {
 		while(true){
 			jokerA();
@@ -237,7 +237,7 @@ public class Solitaire {
 			}
 		}
 	}
-	
+
 	/**
 	 * Utility method that prints a circular linked list, given its rear pointer
 	 * 
@@ -279,9 +279,9 @@ public class Solitaire {
 				encrypted = encrypted + c;
 			}
 		}
-	    return encrypted;
+		return encrypted;
 	}
-	
+
 	/**
 	 * Decrypts a message, which consists of upper case letters only
 	 * 
@@ -305,6 +305,6 @@ public class Solitaire {
 				dencrypted = dencrypted + c;
 			}
 		}
-	    return dencrypted;
+		return dencrypted;
 	}
 }

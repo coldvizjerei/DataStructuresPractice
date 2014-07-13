72058a92d8db0e15186ff28ed5e10e6702e02cda
Stephen Kuo <kuo.stephen72790@gmai.com>
2014-07-13 10:28:21 -0400

    added error checks for inputs

--- src/solitaire/Messenger.java
+++ src/solitaire/Messenger.java
@@ -7,22 +7,33 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Messenger {
-	public static void main(String[] args) 
-	throws IOException {
+	public static void main(String[] args) throws IOException {
 		
 		Solitaire ss = new Solitaire();
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("You can press return to quit anytime during the program");
 		System.out.print("Enter deck file name => ");
 		
-		Scanner sc = new Scanner(new File(br.readLine()));
+		String deck = br.readLine();
+		if (deck.length() == 0){
+			System.exit(0);
+		}
+		
+		//still needs error check for deck potentially not existing
+		Scanner sc = new Scanner(new File(deck));		
 		ss.makeDeck(sc);
 		
 		System.out.print("Encrypt or decrypt? (e/d), press return to quit => ");
 		String inp = br.readLine();
-		if (inp.length() == 0) {
-			System.exit(0);
+		while( (!(inp.charAt(0) == 'e')) && (!(inp.charAt(0) == 'd')) ){
+			if (inp.length() == 0) {
+				System.exit(0);
+			}
+			System.out.print("Please enter either 'e' or 'd' => ");
+			inp = br.readLine();
 		}
+				
 		char ed = inp.charAt(0);
 		System.out.print("Enter message => ");
 		String message = br.readLine();
